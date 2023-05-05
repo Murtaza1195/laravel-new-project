f8552311c4254abe8e07e9a52d804f3a0d6d8bb2
Murtaza1195 <108726538+Murtaza1195@users.noreply.github.com>
2023-05-05 04:45:03 -0700

    Updated README.md  with new content

---
+++ resources/views/ticket/create.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <h1 class="text-white text-lg font-bold ">Create new support Ticket </h1>
+        <div class="w-full sm:max-w-xl mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <form method="post">
+                <div class="mt-4">
+                    <x-input-label for="Title" :value="__('Title')" />
+
+                    <x-text-input id="title" class="block mt-1 w-full"
+                                    type="text"
+                                    name="title"
+                                     required autofocus/>
+
+                    <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                </div>
+
+                <div class="mt-4">
+                    <x-input-label for="Description" :value="__('Description')" />
+                     <x-text-area id="description"  name="description" />
+
+                    <x-input-error :messages="$errors->get('description')" class="mt-2" />
+                </div>
+
+                <div class="mt-4">
+                    <x-input-label for="Attachment" :value="__('Attachment')" />
+
+                    <x-file-input name="attachment" id="attachment"/>
+
+                    <x-input-error :messages="$errors->get('attachment')" class="mt-2" />
+                </div>
+
+
+
+            </form>
+
+        </div>
+
+    </div>
+
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('welcome');
+
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+Route::middleware('auth')->group(function(){
+    //This is the way to call single controller one by one
+    // Route::get('/ticket/create' , [TicketController::class, 'create'])->name('ticket.create');
+    // Route::post('/ticket/create' , [TicketController::class, 'store'])->name('ticket.store');
+    //But as we use resouce controller we just use define in single line by use resource
+    Route::resource('ticket', TicketController::class);
+});
+
+
+require __DIR__.'/auth.php';
